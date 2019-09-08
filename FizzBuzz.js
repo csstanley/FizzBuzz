20c07c7db98674a8274df54412f8ee724f1fca5e
Christopher <csstanley84@gmail.com>
2019-09-08 19:37:05 -0400

    Set up the Conditional

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,13 @@
-for (i = 1; 1>=100; i++) {
-    
+for (i = 1; 1 >= 100; i++) {
+
+    if () {
+        console.log("Fizz");
+
+    } else if () {
+        console.log("Buzz");
+
+    } else if () {
+        console.log(i);
+    }
+
 }
