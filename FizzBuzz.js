7f0081830c99c0f6d0cbe49ffc7862861e83b2e3
Christopher <csstanley84@gmail.com>
2019-09-09 12:54:55 -0400

    FizzBuzz jpg

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,8 +3,8 @@ for (var i = 1; i <= 100; i++) {
     // Creating an variable to represent an empty string //
 var answer = "";
 
-// The % assignment operator returns the remainder. If it returns 0, it's divisible by either 3, 5, or both. //
-// It will return either "Fizz", "Buzz", or "FizzBuzz" by adding the empty string to the string on the right. // 
+// The % assignment operator returns the remainder. If the remainder is 0, it's divisible by either 3, 5, or both. //
+// It will return either "Fizz", "Buzz", or "FizzBuzz" by adding the empty string (answer) to the string on the right. // 
 if (i % 3 == 0) { answer += "Fizz"; }
 if (i % 5 == 0) { answer += "Buzz"; }
 
