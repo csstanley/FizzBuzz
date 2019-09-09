a98ab65cf632cc2b51a9b8297b233b985b6b79ab
Christopher <csstanley84@gmail.com>
2019-09-09 12:10:48 -0400

    Almost Working

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,14 @@
-for (i = 1; 1 >= 100; i++) {
+for (var i = 1; i <= 100; i++) {
 
-    if () {
+        // The  % returns the remainder, so if the remainder is 0, it's divisible by 3 //
+    if (i % 3 == 0) {
         console.log("Fizz");
 
-    } else if () {
+    } else if (i % 5 == 0) {
         console.log("Buzz");
 
-    } else if () {
+        // If it's neither divisible by 3 or 5, return the number //
+    } else if ((i % 3 != 0) && (i % 5 != 0)) {
         console.log(i);
     }
 
